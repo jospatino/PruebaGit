4ba61fa37f5af00c608dcd82c4b2e8dab07f88b2
Patino <jospatinosoli@icloud.com>
2020-12-10 12:43:37 -0600

    Proyecto

---
+++ benitoJuarez/src/main/java/com/benitoJuarez/escolar/BenitoJuarezApplication.java
@@ -0,0 +1,13 @@
+package com.benitoJuarez.escolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenitoJuarezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenitoJuarezApplication.class, args);
+	}
+
+}
---
+++ benitoJuarez/src/test/java/com/benitoJuarez/escolar/BenitoJuarezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benitoJuarez.escolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenitoJuarezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
