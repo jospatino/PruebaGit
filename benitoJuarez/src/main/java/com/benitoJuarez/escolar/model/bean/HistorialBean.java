3c9ad9bf25c5e2e2fe2f0f1998bc3700c249c4e1
victorman93 <cois_fda@hotmail.com>
2020-12-10 14:58:39 -0600

    HistorialBean

--- benitoJuarez/src/main/java/com/benitoJuarez/escolar/model/bean/HistorialBean.java
+++ benitoJuarez/src/main/java/com/benitoJuarez/escolar/model/bean/HistorialBean.java
@@ -1,5 +1,77 @@
 package com.benitoJuarez.escolar.model.bean;
 
-public class HistorialBean {
+import java.io.Serializable;
 
+import javax.validation.constraints.NotNull;
+
+public class HistorialBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@NotNull
+	private int idHistorial;
+	
+	@NotNull
+	private int idPersonal;
+	
+	@NotNull
+	private int idAlumno;
+	
+	@NotNull
+	private int idEscolar;
+	
+	@NotNull
+	private double promedio;
+	
+	public HistorialBean() {
+		super();
+	}
+
+	public HistorialBean(int idHistorial) {
+		super();
+		this.idHistorial = idHistorial;
+	}
+	
+	public int getIdHistorial() {
+		return idHistorial;
+	}
+
+	public void setIdHistorial(int idHistorial) {
+		this.idHistorial = idHistorial;
+	}
+
+	public int getIdPersonal() {
+		return idPersonal;
+	}
+
+	public void setIdPersonal(int idPersonal) {
+		this.idPersonal = idPersonal;
+	}
+
+	public int getIdAlumno() {
+		return idAlumno;
+	}
+
+	public void setIdAlumno(int idAlumno) {
+		this.idAlumno = idAlumno;
+	}
+
+	public int getIdEscolar() {
+		return idEscolar;
+	}
+
+	public void setIdEscolar(int idEscolar) {
+		this.idEscolar = idEscolar;
+	}
+
+	public double getPromedio() {
+		return promedio;
+	}
+
+	public void setPromedio(double promedio) {
+		this.promedio = promedio;
+	}	
 }
