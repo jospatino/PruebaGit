f36f56f71cd16625ce79bec55ebc2d6aad2f98f9
ChristopherGas <paul.rendon.2396@gmail.com.com>
2020-12-11 13:04:37 -0600

    DepartamentoController

---
+++ benitoJuarez/src/main/java/com/benitoJuarez/escolar/controller/DepartamentoController.java
@@ -0,0 +1,53 @@
+package com.benitoJuarez.escolar.controller;
+
+import java.util.*;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.benitoJuarez.escolar.model.bean.DepartamentoBean;
+import com.benitoJuarez.escolar.service.DepartamentoService;
+
+@RestController
+@RequestMapping("/departamento")
+public class DepartamentoController {
+	
+	@Autowired
+	DepartamentoService deptoService;
+	
+	@PostMapping("/crear")
+	public ResponseEntity<Integer> crearDepartamento(@Valid @RequestBody DepartamentoBean deptoBean){
+		return new ResponseEntity<>(this.deptoService.crearDepartamento(deptoBean),HttpStatus.OK);
+	}
+	
+	@GetMapping("/buscar/{idDepartamento}")
+	public ResponseEntity<DepartamentoBean> buscarDepartamento(@PathVariable("idDepartamento")Integer idDepartamento){
+		return new ResponseEntity<>(this.deptoService.getDepartamento(idDepartamento),HttpStatus.OK);
+	}
+	
+	@GetMapping("/buscar")
+	public ResponseEntity<List<DepartamentoBean>> buscar(){
+		return new ResponseEntity<>(this.deptoService.getDepartamento(),HttpStatus.OK);
+	}
+	
+	@PutMapping("/editar")
+	public ResponseEntity<Boolean> editar(@Valid @RequestBody DepartamentoBean deptoBean){
+		return new ResponseEntity<>(this.deptoService.updateDepartamento(deptoBean),HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/eliminar/{idDepartamento}")
+	public ResponseEntity<Boolean> eliminarDeartamento(@PathVariable("idDepartamento") Integer idDepartamento){
+		return new ResponseEntity<>(this.deptoService.deleteDepartamento(idDepartamento),HttpStatus.OK);
+	}
+}
