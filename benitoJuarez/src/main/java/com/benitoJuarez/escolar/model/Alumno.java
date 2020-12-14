bc29921d390ff8c56a6236a0f1832d01733910ec
itzelMirand <itzelmiranda11@hotmail.com>
2020-12-14 09:37:09 -0600

    Tutor

--- benitoJuarez/src/main/java/com/benitoJuarez/escolar/model/Alumno.java
+++ benitoJuarez/src/main/java/com/benitoJuarez/escolar/model/Alumno.java
@@ -4,9 +4,12 @@ import java.sql.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -29,6 +32,10 @@ public class Alumno {
 	private boolean deudorAlumno;
 	@Column(name = "beca", nullable = false)
 	private boolean becaAlumno;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "idTutor")
+	private Tutor tutor;
 	
 	public Alumno() {
 		super();
